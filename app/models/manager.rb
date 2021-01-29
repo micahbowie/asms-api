5aba2c19b2a8ebd20b3154b5c6e7f6376d022547
Micah Bowie <micahbowie20@gmail.com>
2021-01-29 00:04:09 -0500

    building manager model. Adding relationships and validations.

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -1,2 +1,22 @@
 class Manager < ApplicationRecord
+  has_secure_password
+  has_many :employees
+
+  validates_presence_of :first_name, :last_name, :email, :phone
+
+  validates :username, presence: true, uniqueness: true,
+     :format => {
+     with: /[a-zA-Z0-9]+\z/ ,
+     :message => 'no special characters, only letters and numbers'
+     }
+ validates_length_of :username, minimum: 5, maximum: 12, allow_blank: false
+
+ validates :first_name, format: {
+  with: /[a-zA-Z]+\z/,
+  message: 'no special characters or numbers, only letters'
+}
+validates :last_name, format: {
+ with: /[a-zA-Z]+\z/,
+ message: 'no special characters or numbers, only letters'
+}
 end
